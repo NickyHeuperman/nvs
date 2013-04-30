3ba6da9b3f6c28878439d64f7ceac4103d5ee229
Nicky Heuperman <nicky@dev-ice.com>
2013-05-01 00:18:46 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NovusDominatum::Application.config.secret_token = 'a569c45df24b3b0fcc28c593259a34046fb216796be878be197cdc3669495ca7bf6c2aaf5d5a46c32ca5ada6896423371845fecba654bf7c0d59a5c51cd0b603'
