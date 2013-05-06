f2ad165da06c0956f5ed03715ca4793402828de8
Nicky Heuperman <nicky@dev-ice.com>
2013-05-06 16:01:11 +0200

    make kill statistic support week and year numbers

---
+++ app/models/yapeal_database.rb
@@ -0,0 +1,55 @@
+class YapealDatabase < ActiveRecord::Yapeal
+	set_table_name "yap_corpVictim"
+	def self.getRecentKills(amount)
+		YapealDatabase.find_by_sql(["SELECT yap_corpVictim.characterName AS victim,yap_corpVictim.killID AS killID,yap_corpVictim.shipTypeID as typeID,yap_corpVictim.characterID as victimID,yap_corpVictim.corporationID as victimCorpID,yap_corpVictim.corporationName as victimCorp, yap_corpAttackers.characterName AS attacker,yap_corpAttackers.characterID as attackerID,yap_corpAttackers.shipTypeID as attackerShipID, yap_corpKillLog.killID, EveDataDump.invTypes.typeName as shipName
+			FROM yap_corpKillLog
+			INNER JOIN yap_corpVictim ON yap_corpVictim.killID = yap_corpKillLog.killID
+			INNER JOIN yap_corpAttackers ON yap_corpAttackers.killID = yap_corpKillLog.killID
+		INNER JOIN EveDataDump.invTypes on EveDataDump.invTypes.typeID=yap_corpVictim.shipTypeID
+			WHERE yap_corpVictim.allianceID <>  99000203
+			AND yap_corpAttackers.allianceID =  99000203
+			AND yap_corpAttackers.finalBlow =1
+		ORDER BY  `yap_corpKillLog`.`killTime` DESC  LIMIT ?",amount])
+	end
+	def self.getShipClasses()
+		YapealDatabase.find_by_sql(
+		"select EveDataDump.invTypes.groupID as groupID, EveDataDump.invGroups.groupName as groupName FROM EveDataDump.invGroups
+INNER JOIN EveDataDump.invTypes on EveDataDump.invTypes.groupID=EveDataDump.invGroups.groupID
+WHERE(EveDataDump.invGroups.categoryID=6 OR
+EveDataDump.invGroups.categoryID=23)
+GROUP BY EveDataDump.invTypes.groupID
+ORDER BY  EveDataDump.invGroups.categoryID,EveDataDump.invGroups.groupName ASC")
+	end
+	def self.getWeekKillCount(year,week)
+	
+		YapealDatabase.find_by_sql(["SELECT EveDataDump.invTypes.groupID as groupID, EveDataDump.invGroups.groupName as groupName ,count(yap_corpKillLog.killID) as count,CAST(SUM(if(yap_corpVictim.allianceID =  99000203 AND yap_corpAttackers.finalBlow =1 AND 
+(WEEK(yapeal.yap_corpKillLog.killTime) <=> ?
+AND YEAR(yapeal.yap_corpKillLog.killTime) <=> ?), 1, 0))AS SIGNED) AS losses,
+CAST(SUM(if(yap_corpVictim.allianceID <>  99000203 AND yap_corpAttackers.finalBlow =1 AND 
+(WEEK(yapeal.yap_corpKillLog.killTime) <=> ?
+AND YEAR(yapeal.yap_corpKillLog.killTime) <=> ?), 1, 0))AS SIGNED) AS kills,yap_corpVictim.allianceID as allianceID
+FROM EveDataDump.invGroups
+INNER JOIN EveDataDump.invTypes on EveDataDump.invTypes.groupID=EveDataDump.invGroups.groupID
+LEFT JOIN yapeal.yap_corpVictim on yapeal.yap_corpVictim.shipTypeID=EveDataDump.invTypes.typeID
+LEFT JOIN yapeal.yap_corpKillLog ON yapeal.yap_corpVictim.killID = yapeal.yap_corpKillLog.killID
+LEFT JOIN yapeal.yap_corpAttackers ON yapeal.yap_corpAttackers.killID = yapeal.yap_corpKillLog.killID
+WHERE(EveDataDump.invGroups.categoryID=6 OR
+EveDataDump.invGroups.categoryID=23)
+
+GROUP BY EveDataDump.invGroups.groupName
+		ORDER BY  EveDataDump.invGroups.categoryID,EveDataDump.invGroups.groupName ASC",week,year,week,year])
+	end
+	def self.getOverallKillCount()
+		YapealDatabase.find_by_sql("SELECT EveDataDump.invTypes.groupID as groupID, EveDataDump.invGroups.groupName as groupName ,count(yap_corpKillLog.killID) as count,CAST(SUM(if(yap_corpVictim.allianceID =  99000203, 1, 0)) AS SIGNED) AS losses,CAST(SUM(if(yap_corpVictim.allianceID <>  99000203, 1, 0))AS SIGNED) AS kills
+FROM EveDataDump.invGroups
+INNER JOIN EveDataDump.invTypes on EveDataDump.invTypes.groupID=EveDataDump.invGroups.groupID
+LEFT JOIN yapeal.yap_corpVictim on yapeal.yap_corpVictim.shipTypeID=EveDataDump.invTypes.typeID
+LEFT JOIN yapeal.yap_corpKillLog ON yapeal.yap_corpVictim.killID = yapeal.yap_corpKillLog.killID
+LEFT JOIN yapeal.yap_corpAttackers ON yapeal.yap_corpAttackers.killID = yapeal.yap_corpKillLog.killID
+WHERE (EveDataDump.invGroups.categoryID=6 OR
+EveDataDump.invGroups.categoryID=23)
+GROUP BY EveDataDump.invTypes.groupID
+		ORDER BY  EveDataDump.invGroups.categoryID,EveDataDump.invGroups.groupName ASC")
+	end
+
+end
